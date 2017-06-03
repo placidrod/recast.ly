8ab1bb6bd5e8c3c533367cd901bff989d49fc21d
Placid Rodrigues <placidrod@gmail.com>
2017-06-03 12:26:13 -0700

    Get live youtube data

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    // contentType: 'application/json',
+    data: {
+      part: 'snippet',
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      videoEmbeddable: 'true',
+      type: 'video'
+    },
+    success: function(response) {
+      console.log(response);
+      callback(response.items);
+    },
+    error: function(e) {
+      console.log('error', e);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
